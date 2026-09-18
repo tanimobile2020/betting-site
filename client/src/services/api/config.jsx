aa9c4850bf895a3d9437dde4f76a81c5accb213e
tanimobile2020 <151889468+tanimobile2020@users.noreply.github.com>
2026-09-18 23:42:30 +0200

    Update config.jsx

--- client/src/services/api/config.jsx
+++ client/src/services/api/config.jsx
@@ -1,4 +1,4 @@
 export const API_ENDPOINTS = {
-  ACCOUNTS: "https://betting-site-0hci.onrender.com/accounts/api",
-  SPORTS: "https://betting-site-0hci.onrender.com/sports/api",
+  ACCOUNTS: "https://betting-site-0hci.onrender.com/accounts/api/",
+  SPORTS: "https://betting-site-0hci.onrender.com/sports/api/",
 }
